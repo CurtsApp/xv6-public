ab6e59d5afb0c726b17c8bfbbed6df745ec935a2
CurtsApp <curtmsapp@gmail.com>
2018-10-23 20:35:56 -0700

    Completed implementation of CAS from Assignment 2. Using the echo command to test functionality.

--- echo.c
+++ echo.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "x86.h"
 
 int
 main(int argc, char *argv[])
@@ -9,7 +10,15 @@ main(int argc, char *argv[])
 
   for(i = 1; i < argc; i++)
     printf(1, "%s%s", argv[i], i+1 < argc ? " " : "\n");
+  int a = 2;
   
-  printf(1, "%s", "Test on Curt branch v2");
+  printf(1, "Value of a: %d\n", a);
+  // cas(volatile int *addr, int expected, int newval)
+  cas(&a, 1, 3);
+  printf(1, "Value of a: %d\n", a);
+  cas(&a, 2, 4);
+  printf(1, "Value of a: %d\n", a);
+  cas(&a, 2, 5);
+  printf(1, "Value of a: %d\n", a);
   exit();
 }
